7a24ee30a6846ad8be9ce6bd8cd984e497feb44f
Mr-Sunny-0v0 <zihao0v0@163.com>
2024-02-23 20:45:57 +0800

    C语言实现经典程序---2

--- C_classic_program/C_classic_program/classic_program.c
+++ C_classic_program/C_classic_program/classic_program.c
@@ -2,6 +2,8 @@
 
 
 
+
+
 //1.加法器与累乘器
 //介绍：加法器实现累加操作；累乘器实现累乘操作
 
@@ -53,7 +55,30 @@
 
 
 
+//2.求最大数与最小数
+//介绍：求出一组数据中的最大值和最小值
 
+//#include <stdio.h>
+//
+//int main(){
+//	int i,min,max,num[5];
+//	printf("请输入5个数（用空格隔开）：");
+//	for(i=0;i<5;i++){
+//		scanf("%d",&num[i]);
+//	}
+//	min = num[0];
+//	max = num[0];
+//	for(i=1;i<5;i++){
+//		if(min > num[i]){
+//			min = num[i];
+//		}
+//		if(max < num[i]){
+//			max = num[i];
+//		}
+//	}
+//	printf("min = %d\nmax = %d\n",min,max);
+//	return 0;
+//}
 
 
 
